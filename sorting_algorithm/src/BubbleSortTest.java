86098979f5a9a4345fed12558c16e63c01027760
yuehanli1 <yuehan.li@thoughtworks.com>
2022-11-23 14:48:40 +0800

    feat: bubble sort for int arrays

---
+++ sorting_algorithm/src/BubbleSortTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BubbleSortTest {
+
+    @Test
+    void sort() {
+        int[] sourceArrays = {1, 3, 4, 6, 7, 5, 8};
+        int[] targetArrays  = {1, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(targetArrays, new BubbleSort().sort(sourceArrays));
+    }
+}
