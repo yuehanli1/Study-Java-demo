450648aa7921f11cf191dd3c142af4852dab403c
yuehanli1 <yuehan.li@thoughtworks.com>
2022-11-24 15:15:07 +0800

    feat: java 8 feature reflection for dynamic proxy

--- java8_feature/src/reflectionDynamicProxyDemo.java
+++ java8_feature/src/reflectionDynamicProxyDemo.java
@@ -3,7 +3,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class reflectionDynamicProxyDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         InvocationHandler handler = new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -21,10 +21,26 @@ public class reflectionDynamicProxyDemo {
                 new Class[] {Hello.class},
                 handler
         );
+
+        System.out.println("--------------implements----------");
         hello.morning("morning");
+        HelloDynamicProxy dynamicProxy = new HelloDynamicProxy(handler);
+        dynamicProxy.morning("morning");
     }
 }
 
 interface Hello {
-    void morning(String name);
+    void morning(String name) throws Throwable;
+}
+
+class HelloDynamicProxy implements Hello {
+    InvocationHandler handler;
+
+    public HelloDynamicProxy(InvocationHandler handler) {
+        this.handler = handler;
+    }
+
+    public void morning(String name) throws Throwable {
+        handler.invoke(this, Hello.class.getMethod("morning", String.class), new Object[] {name});
+    }
 }
